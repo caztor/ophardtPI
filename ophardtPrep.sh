9373925ad9e6e139ec1f36dd3eaec866581c06a1
Theis <23695072+caztor@users.noreply.github.com>
2020-05-08 23:26:32 +0200

    Update ophardtPrep.sh

--- ophardtPrep.sh
+++ ophardtPrep.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
+echo 'This will install Ophardt on your Raspberry Pi 4b'
+
+PS3='Please choose desired webserver: '
+options=("Option 1" "Option 2" "Option 3" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Apache2")
+            echo "Installing Ophardt on $opt"
+            SERVER = $opt
+            break
+            ;;
+        "Nginx")
+            echo "Installing Ophardt on $opt"
+            SERVER = $opt
+            break
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
+
 #Make sure that Raspbian and packages are up to date
 apt update
 apt upgrade -y
 
 #Install Apache, PHP, PHP Extensions, MariaDB, MySQL Python Connector, Java
-apt install apache2 php7.2 mariadb-server php7.2-mysql php7.2-xml php7.2-curl php7.2-intl php7.2-zip php7.2-mbstring python-mysql.connector openjdk-8-jre ufw certbot python-certbot-apache -y
+apt install mariadb-server python-mysql.connector openjdk-8-jre ufw certbot python-certbot-apache -y
 
 #Configure and Enable firewall
 ufw allow 'OpenSSH'
@@ -16,30 +40,42 @@ echo '[mysqld]
 sql-mode=ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' >> /etc/mysql/conf.d/mysql.cnf
 service mysqld restart
 
-#Create secure SSL config for Apache
-cat > /etc/apache2/conf-available/ssl-params.conf << EOF
-SSLCipherSuite EECDH+AESGCM:EDH+AESGCM:AES256+EECDH:AES256+EDH
-SSLProtocol All -SSLv2 -SSLv3 -TLSv1 -TLSv1.1
-SSLHonorCipherOrder On
-# Disable preloading HSTS for now.  You can use the commented out header line that includes
-# the "preload" directive if you understand the implications.
-# Header always set Strict-Transport-Security "max-age=63072000; includeSubDomains; preload"
-Header always set X-Frame-Options DENY
-Header always set X-Content-Type-Options nosniff
-# Requires Apache >= 2.4
-SSLCompression off
-SSLUseStapling on
-SSLStaplingCache "shmcb:logs/stapling-cache(150000)"
-# Requires Apache >= 2.4.11
-SSLSessionTickets Off
-EOF
+if [ $SERVER -eq 'Apache2' ]; then
 
-#Enable apache modules and sites
-sudo a2enmod rewrite
-sudo a2enmod ssl
-sudo a2enmod headers
-sudo a2ensite default-ssl
-sudo a2enconf ssl-params
+  #Install Apache, PHP, PHP Extensions, MariaDB, MySQL Python Connector, Java
+  apt install apache2 php7.2 php7.2-mysql php7.2-xml php7.2-curl php7.2-intl php7.2-zip php7.2-mbstring -y
+
+  #Create secure SSL config for Apache
+  cat > /etc/apache2/conf-available/ssl-params.conf << EOF
+  SSLCipherSuite EECDH+AESGCM:EDH+AESGCM:AES256+EECDH:AES256+EDH
+  SSLProtocol All -SSLv2 -SSLv3 -TLSv1 -TLSv1.1
+  SSLHonorCipherOrder On
+  # Disable preloading HSTS for now.  You can use the commented out header line that includes
+  # the "preload" directive if you understand the implications.
+  # Header always set Strict-Transport-Security "max-age=63072000; includeSubDomains; preload"
+  Header always set X-Frame-Options DENY
+  Header always set X-Content-Type-Options nosniff
+  # Requires Apache >= 2.4
+  SSLCompression off
+  SSLUseStapling on
+  SSLStaplingCache "shmcb:logs/stapling-cache(150000)"
+  # Requires Apache >= 2.4.11
+  SSLSessionTickets Off
+  EOF
+
+  #Enable apache modules and sites
+  sudo a2enmod rewrite
+  sudo a2enmod ssl
+  sudo a2enmod headers
+  sudo a2ensite default-ssl
+  sudo a2enconf ssl-params
+
+elif [ $SERVER -eq 'Nginx' ]; then
+
+  #Install Apache, PHP, PHP Extensions, MariaDB, MySQL Python Connector, Java
+  apt install nginx php7.2-fpm php7.2-mysql php7.2-xml php7.2-curl php7.2-intl php7.2-zip php7.2-mbstring -y
+
+fi
 
 #Secure MySQL installation
 mysql_secure_installation

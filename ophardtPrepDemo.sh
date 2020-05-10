a496d0632b9115d3420b496f4313b3ba39170108
Theis <23695072+caztor@users.noreply.github.com>
2020-05-10 15:15:10 +0200

    Update ophardtPrepDemo.sh

--- ophardtPrepDemo.sh
+++ ophardtPrepDemo.sh
@@ -10,18 +10,27 @@ echo Where should the domain name be?
 read -p 'Input domain suffix (i.e. example.com): ' demodomain
 
 #Remove existing installation
-#rm /var/www/student*
+#rm -f /var/www/student*
 
-cat > /etc/nginx/sites-available/student1.$demodomain << EOF
-# Virtual Host configuration for $demodomain
+rm -f /etc/nginx/sites-available/student*
+rm -f /etc/nginx/sites-enabled/student*
+
+for demo in {1..$democount}
+do
+	echo Preparing demo environment student$demo.$demodomain
+	#cp -R /var/www/fencing /var/www/student$demo
+	sed -i 's/score_fencing/student$demo/g' /var/www/student$demo/app/config/parameters.yml
+
+	cat > /etc/nginx/sites-available/student$demo.$demodomain << EOF
+# Virtual Host configuration for student$demo.$demodomain
 
 server {
 	listen 80;
 	listen [::]:80;
 
-	server_name student1.$demodomain;
+	server_name student$demo.$demodomain;
 
-	root /var/www/student1;
+	root /var/www/student$demo;
 	index app.php;
 
 	location / {
@@ -29,3 +38,7 @@ server {
 	}
 }
 EOF
+
+	sudo ln -s /etc/nginx/sites-available/student$demo.$demodomain /etc/nginx/sites-enabled/student$demo.$demodomain
+
+done
